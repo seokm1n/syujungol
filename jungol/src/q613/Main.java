3e5f029beed00b014ef25316b6310ed72cd280ec
suckm <suckm@DESKTOP-DUMRNUO>
2024-12-31 17:04:00 +0900

    q614

--- jungol/src/q613/Main.java
+++ jungol/src/q613/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int i = 0;
+		String name = sc.next();
+		String school = sc.next();
+		int grade = sc.nextInt();
+		Student s1 = new Student(name, school, grade);
+		sc.close();
+		
+		s1.printInfo();
 	}
 }
